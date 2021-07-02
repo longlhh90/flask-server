b6271846df2026081c425efbe0a7374e68f2a5af
Long Le <longlhh90@google.com>
2021-07-01 23:31:06 -0600

    [settings] add settings for JWT

--- settings/common.py
+++ settings/common.py
@@ -1,5 +1,5 @@
 import os
-from datetime import datetime, timedelta
+from datetime import datetime, time, timedelta
 
 # FLASK CONFIG
 SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", None)
@@ -13,3 +13,9 @@ API_KEY = os.environ.get("API_KEY", None)
 SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 PROPAGATE_EXCEPTIONS = True
+
+# JWT AUTH CONFIG
+JWT_AUTH_USERNAME_KEY = "email"
+JWT_AUTH_PASSWORD_KEY = "password"
+JWT_EXPIRATION_DELTA = timedelta(seconds=300)
+JWT_NOT_BEFORE_DELTA = timedelta(seconds=0)
